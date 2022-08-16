8065915dc1d004cb2e130d01abb28c3b463366d8
Robert Bartel <robert.bartel@noaa.gov>
2022-08-16 15:35:50 -0500

    Update DmodClient for following status updates.
    In support of long-running websocket connections that start a job and
    then follow its progression.

--- python/lib/client/dmod/client/dmod_client.py
+++ python/lib/client/dmod/client/dmod_client.py
@@ -1,10 +1,18 @@
+import logging
+
+from dmod.communication import UpdateMessage
 from dmod.core.execution import AllocationParadigm
-from dmod.core.meta_data import DataCategory, DataDomain, DataFormat, DiscreteRestriction
-from .request_clients import DatasetClient, DatasetExternalClient, DatasetInternalClient, NgenRequestClient
+from dmod.core.exception import DmodRuntimeError
+from dmod.core.meta_data import DataCategory, DataDomain, DataFormat, DiscreteRestriction, TimeRange
+from dmod.core.serializable import BasicResultIndicator, ResultIndicator
+from .request_clients import DatasetClient, DatasetExternalClient, DatasetInternalClient, FollowableClient, \
+    NgenRequestClient
 from .client_config import YamlClientConfig
 from datetime import datetime
 from pathlib import Path
-from typing import List, Optional
+from typing import Dict, List, Optional, Set
+import asyncio
+import uuid
 
 
 class DmodClient:
@@ -14,6 +22,10 @@ class DmodClient:
         self._dataset_client = None
         self._ngen_client = None
         self._bypass_request_service = bypass_request_service
+        self._followed_jobs: Dict[str, Set[uuid.UUID]] = dict()
+        """ Job id keyed to sets of registered update queue ids. """
+        self._job_update_queues: Dict[uuid.UUID, asyncio.Queue[UpdateMessage]] = dict()
+        """ Update queue ids to update queues for updates on jobs that are being followed. """
 
     @property
     def client_config(self):
@@ -112,6 +124,31 @@ class DmodClient:
     async def download_from_dataset(self, dataset_name: str, item_name: str, dest: Path) -> bool:
         return await self.dataset_client.download_from_dataset(dataset_name=dataset_name, item_name=item_name,
                                                                dest=dest)
+
+    async def get_followed_update(self, unique_queue_id: uuid.UUID) -> UpdateMessage:
+        """
+        Get the next queued update message in the identified queue, or await one to be received.
+
+        The unique id must have come from registering to follow updates for a particular job through
+        ::method:`register_follow_updates`.
+
+        Parameters
+        ----------
+        unique_queue_id : uuid.UUID
+            The unique id for the update queue to listen through, received when registering for updates for a job.
+
+        Returns
+        -------
+        UpdateMessage
+            The next available update, either immediately if any are queued, or once the next one is received.
+        See Also
+        -------
+        queue_updates
+        register_follow_updates
+        """
+        if unique_queue_id not in self._job_update_queues:
+            raise DmodRuntimeError("Unrecognized update queue identifier to follow: {}".format(str(unique_queue_id)))
+        return await self._job_update_queues[unique_queue_id].get()
 
     async def list_datasets(self, category: Optional[DataCategory] = None):
         return await self.dataset_client.list_datasets(category)
@@ -229,16 +266,129 @@ class DmodClient:
     def requests_ssl_dir(self) -> Path:
         return self.client_config.requests_ssl_dir
 
+    def register_follow_updates(self, job_id: str) -> uuid.UUID:
+        """
+        Register to follow updates for a running job.
+
+        Parameters
+        ----------
+        job_id : str
+            The job id of interest.
+
+        Returns
+        -------
+        uuid.UUID
+            The unique queue id of the queue from which this job's followed updates can be retrieved.
+
+        See Also
+        -------
+        queue_updates
+        get_followed_update
+        unregister_follow_updates
+        """
+        if job_id not in self._followed_jobs:
+            raise DmodRuntimeError("Can't register {} to follow unknown job {}".format(self.__class__.__name__, job_id))
+        unique_queue_id = uuid.uuid4()
+        self._followed_jobs[job_id].add(unique_queue_id)
+        self._job_update_queues[unique_queue_id] = asyncio.Queue()
+        return unique_queue_id
+
     async def submit_ngen_request(self, start: datetime, end: datetime, hydrofabric_data_id: str, hydrofabric_uid: str,
                                   cpu_count: int, realization_cfg_data_id: str, bmi_cfg_data_id: str,
                                   partition_cfg_data_id: Optional[str] = None, cat_ids: Optional[List[str]] = None,
-                                  allocation_paradigm: Optional[AllocationParadigm] = None, *args, **kwargs):
-        return await self.ngen_request_client.request_exec(start, end, hydrofabric_data_id, hydrofabric_uid,
-                                                           cpu_count, realization_cfg_data_id, bmi_cfg_data_id,
-                                                           partition_cfg_data_id, cat_ids, allocation_paradigm)
+                                  allocation_paradigm: Optional[AllocationParadigm] = None, *args,
+                                  **kwargs) -> ResultIndicator:
+        # TODO: document this keyword arg
+        follow_locally = kwargs.get('follow_updates_locally', False)
+        time_range = TimeRange(begin=start, end=end)
+        request_obj = await self.ngen_request_client.build_message(time_range, hydrofabric_data_id, hydrofabric_uid,
+                                                                   cpu_count, realization_cfg_data_id, bmi_cfg_data_id,
+                                                                   partition_cfg_data_id, cat_ids, allocation_paradigm)
+        # Create runtime context to make sure the client's connection is in place
+        with self.ngen_request_client:
+            job_response = await self.ngen_request_client.request_exec(request=request_obj)
+            if not job_response.success:
+                return job_response
+            job_id = job_response.job_id
+            logging.info('Received successful response from job request, with job id {}'.format(job_id))
+            # Prepare the set of update queues following updates of this job
+            self._followed_jobs[job_id] = set()
+            # Optionally, set up a queue for this function to also read and print from
+            if follow_locally:
+                unique_queue_id = self.register_follow_updates(job_id)
+            try:
+                # Create async tasks to follow updates within the request client and here
+                client_updates_task = asyncio.create_task(self.ngen_request_client.follow_exec(job_id))
+                follow_updates_task = asyncio.create_task(self.queue_updates(job_id, self.ngen_request_client))
+                # Output updates, if locally we are supposed to and they could still be coming in
+                while follow_locally and self.ngen_request_client.is_following_updates(job_id):
+                    logging.info(str(await self._job_update_queues[unique_queue_id].get()))
+                # Await the completion of the async tasks (i.e., the client following updates) before proceeding
+                await client_updates_task
+                await follow_updates_task
+                # Return the previously received job request response
+                return job_response
+            except Exception as e:
+                msg = 'Job {} successfully requested, but encountered {} while following status updates ({})'
+                return BasicResultIndicator(success=False, reason='Encountered {} Following Updates',
+                                            message=msg.format(job_id, e.__class__.__name__, str(e)))
+            finally:
+                self.unregister_follow_updates(job_id=job_id,
+                                               unique_queue_id=unique_queue_id if follow_locally else uuid.uuid4())
 
     def print_config(self):
         print(self.client_config.config_file.read_text())
+
+    async def queue_updates(self, job_id: str, client: FollowableClient):
+        """
+        Async function for adding updates from a followable client to this instance's update queues.
+
+        Parameters
+        ----------
+        job_id : str
+            The unique id for the job for which updates should be queue.
+        client : FollowableClient
+            The client through which to get updates, which itself is expected to be following updates.
+        """
+        while client.is_following_updates(job_id=job_id) and self._followed_jobs.get(job_id):
+            update_object = await client.get_update_from_queue(job_id=job_id)
+            if job_id in self._followed_jobs:
+                for queue_id, update_queue in self._job_update_queues.items():
+                    await update_queue.put(update_object)
+
+    def unregister_follow_updates(self, job_id: str, unique_queue_id: uuid.UUID):
+        """
+        Unregister to stop following updates for a running job.
+
+        Note that parameter values not found to be registered or expected - an unrecognized ``job_id`` results in steps
+        being skipped but no errors being thrown.  I.e., it is safe to unregister bogus values.
+
+        Parameters
+        ----------
+        job_id : str
+            The job id for which to unregister.
+        unique_queue_id : uuid.UUID
+            The id of the update queue to deregister and remove.
+
+        Returns
+        -------
+        uuid.UUID
+            The unique queue id of the queue from which this job's followed updates can be retrieved.
+
+        See Also
+        -------
+        queue_updates
+        get_followed_update
+        unregister_follow_updates
+        """
+        if unique_queue_id in self._job_update_queues:
+            self._job_update_queues.pop(unique_queue_id)
+
+        if job_id in self._followed_jobs:
+            if unique_queue_id in self._followed_jobs[job_id]:
+                self._followed_jobs[job_id].remove(unique_queue_id)
+            if len(self._followed_jobs[job_id]) == 0:
+                self._followed_jobs.pop(job_id)
 
     async def upload_to_dataset(self, dataset_name: str, paths: List[Path]) -> bool:
         """
