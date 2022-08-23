f6a027cc556ead12a6b686e927f1cbb89caedbc1
Robert Bartel <robert.bartel@noaa.gov>
2022-08-23 15:39:34 -0500

    Move common ngen image script from entrypoint.
    Using functionality now in separated common.sh file in main
    entrypoint.sh.

--- docker/main/ngen/entrypoint.sh
+++ docker/main/ngen/entrypoint.sh
@@ -21,118 +21,13 @@ REALIZATION_CONFIG_DATASET_NAME="${7:?}"
 BMI_CONFIG_DATASET_NAME="${8:?}"
 PARTITION_DATASET_NAME="${9:?}"
 
-ACCESS_KEY_SECRET="object_store_exec_user_name"
-SECRET_KEY_SECRET="object_store_exec_user_passwd"
-DOCKER_SECRETS_DIR="/run/secrets"
-ACCESS_KEY_FILE="${DOCKER_SECRETS_DIR}/${ACCESS_KEY_SECRET}"
-SECRET_KEY_FILE="${DOCKER_SECRETS_DIR}/${SECRET_KEY_SECRET}"
+# Source several common settings/variables and shared functions
+source common.sh
 
-MPI_USER="mpi"
-if [ "$(whoami)" = "${MPI_USER}" ]; then
-    MPI_HOSTS_FILE="${HOME}/.mpi_hosts"
-else
-    MPI_HOSTS_FILE="$(su ${MPI_USER} -c 'echo "${HOME}"')/.mpi_hosts"
-fi
-
-MPI_RUN="mpirun"
-#NGEN_EXECUTABLE="ngen"
-NGEN_EXECUTABLE="/ngen/ngen/cmake_build/ngen"
-
-ALL_DATASET_DIR="/dmod/datasets"
-OUTPUT_DATASET_DIR="${ALL_DATASET_DIR}/output/${OUTPUT_DATASET_NAME}"
-HYDROFABRIC_DATASET_DIR="${ALL_DATASET_DIR}/hydrofabric/${HYDROFABRIC_DATASET_NAME}"
-REALIZATION_CONFIG_DATASET_DIR="${ALL_DATASET_DIR}/config/${REALIZATION_CONFIG_DATASET_NAME}"
-BMI_CONFIG_DATASET_DIR="${ALL_DATASET_DIR}/config/${BMI_CONFIG_DATASET_NAME}"
-PARTITION_DATASET_DIR="${ALL_DATASET_DIR}/config/${PARTITION_DATASET_NAME}"
-
-RUN_SENTINEL="/home/${MPI_USER}/.run_sentinel"
-
-print_date()
-{
-    date "+%Y-%m-%d,%H:%M:%S"
-}
-
-check_for_dataset_dir()
-{
-    # Dataset dir is $1
-    _CATEG="$(echo "${1}" | sed "s|${ALL_DATASET_DIR}/\([^/]*\)/.*|\1|" | awk '{print toupper($0)}')"
-    if [ ! -d "${1}" ]; then
-        echo "Error: expected ${_CATEG} dataset directory ${1} not found." 2>&1
-        exit 1
-    fi
-}
-
-load_object_store_keys_from_docker_secrets()
-{
-    # Read Docker Secrets files for Object Store access, if they exist
-    if [ -z "${ACCESS_KEY_FILE:-}" ]; then
-        echo "WARN: Cannot load object store access key when Docker secret file name not set"
-    elif [ -e "${ACCESS_KEY_FILE}" ]; then
-        ACCESS_KEY="$(cat "${ACCESS_KEY_FILE}")"
-    else
-        echo "WARN: Cannot load object store access key when Docker secret file does not exist"
-    fi
-
-    if [ -z "${SECRET_KEY_FILE:-}" ]; then
-            echo "WARN: Cannot load object store secret key when Docker secret file name not set"
-    elif [ -e "${SECRET_KEY_FILE}" ]; then
-        SECRET_KEY="$(cat "${SECRET_KEY_FILE}")"
-    else
-        echo "WARN: Cannot load object store secret key when Docker secret file does not exist"
-    fi
-
-    test -n "${ACCESS_KEY:-}" && test -n "${SECRET_KEY:-}"
-}
-
-exec_main_worker_ngen_run()
-{
-    # Write (split) hoststring to a proper file
-    if [ -e "${MPI_HOSTS_FILE}" ]; then
-        rm "${MPI_HOSTS_FILE}"
-    fi
-
-    echo "$(print_date) Preparing hosts file and checking ${MPI_NODE_COUNT} worker hosts are online for SSH"
-    for i in $(echo "${MPI_HOST_STRING}" | sed 's/,/ /g'); do
-        #echo "${i}" | awk -F: '{print $1 " slots=" $2}' >> "${MPI_HOSTS_FILE}"
-        echo "${i}" | awk -F: '{print $1 ":" $2}' >> "${MPI_HOSTS_FILE}"
-        _HOST_NAME=$(echo "${i}" | awk -F: '{print $1}')
-        # Make sure all hosts are reachable, this also covers localhost
-        until ssh -q ${_HOST_NAME} exit >/dev/null 2>&1; do :; done
-        echo "DEBUG: Confirmed MPI host ${_HOST_NAME} is online for SSH"
-    done
-
-    # Execute the model on the linked data
-    echo "$(print_date) Executing mpirun command for ngen on ${MPI_NODE_COUNT} workers"
-    ${MPI_RUN:?} -f "${MPI_HOSTS_FILE}" -n ${MPI_NODE_COUNT} \
-        ${NGEN_EXECUTABLE:?} ${HYDROFABRIC_DATASET_DIR}/catchment_data.geojson "" \
-                ${HYDROFABRIC_DATASET_DIR}/nexus_data.geojson "" \
-                ${REALIZATION_CONFIG_DATASET_DIR}/realization_config.json \
-                ${PARTITION_DATASET_DIR}/partition_config.json \
-                --subdivided-hydrofabric
-
-    #Capture the return value to use as service exit code
-    NGEN_RETURN=$?
-
-    echo "$(print_date) ngen mpirun command finished with return value: ${NGEN_RETURN}"
-
-    # Close the other workers by removing this file
-    for i in $(echo "${MPI_HOST_STRING}" | sed 's/,/ /g'); do
-        _HOST_NAME=$(echo "${i}" | awk -F: '{print $1}')
-        ssh -q ${_HOST_NAME} rm ${RUN_SENTINEL} >/dev/null 2>&1
-    done
-
-    echo "$(print_date) DEBUG: closed other worker SSH processes"
-
-    # Exit with the model's exit code
-    return ${NGEN_RETURN}
-}
+set_mpi_host_file_path
 
 # Sanity check that the output, hydrofabric, and config datasets are available (i.e., their directories are in place)
-check_for_dataset_dir "${REALIZATION_CONFIG_DATASET_DIR}"
-check_for_dataset_dir "${BMI_CONFIG_DATASET_DIR}"
-check_for_dataset_dir "${PARTITION_DATASET_DIR}"
-check_for_dataset_dir "${HYDROFABRIC_DATASET_DIR}"
-check_for_dataset_dir "${OUTPUT_DATASET_DIR}"
+check_for_default_dataset_dirs
 
 # Move to the output dataset mounted directory
 cd ${OUTPUT_DATASET_DIR}
