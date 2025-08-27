d3c400ec5dc485141e8e0aa28ad16d1b9c22dc57
Robert Bartel <robert.bartel@noaa.gov>
2025-08-27 16:45:06 -0400

    Add new types to dmod.modeldata.hydrofabric init.

--- python/lib/modeldata/dmod/modeldata/hydrofabric/__init__.py
+++ python/lib/modeldata/dmod/modeldata/hydrofabric/__init__.py
@@ -1,4 +1,5 @@
-from .hydrofabric import Hydrofabric, HydrofabricFilesManager, GeoJsonHydrofabricReader, GeoJsonHydrofabric, \
-    MappedGraphHydrofabric
+from .hydrofabric import (Hydrofabric, HydrofabricFilesManager, GeoJsonHydrofabricReader, GeoJsonHydrofabric,
+                          MappedGraphHydrofabric, HydrofabricCatchment)
 from .partition import Partition, PartitionConfig
-from .geopackage_hydrofabric import GeoPackageHydrofabric
+from .geopackage_hydrofabric import (GeoPackageHydrofabric, Version22Hydrofabric, V22SqliteHydrofabric, V22Catchment,
+                                     V22SqliteCatchment, V22SqliteNexus)
